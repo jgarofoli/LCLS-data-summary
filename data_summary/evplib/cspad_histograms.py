025f06e0c9c21c13fa28f59bd64548b111d9327f
Justin Garofoli <justing@slac.stanford.edu>
2015-03-05 12:48:34 -0800

    working

--- data_summary/evplib/cspad_histograms.py
+++ data_summary/evplib/cspad_histograms.py
@@ -19,6 +19,7 @@ class histo(object):
     def __init__(self,n,bins):
         self._histos = {}
         self._nentries = {}
+        self._axes = {}
         self.bins = bins
         for i in xrange(n):
             self._histos[i] = numpy.histogram([],bins=self.bins)[0]
@@ -35,13 +36,25 @@ class histo(object):
             subplotnum=k%16
             if k % 16 == 0:
                 pylab.figure()
-            pylab.subplot(4,4,subplotnum)
+            self._axes[k] = pylab.subplot(4,4,subplotnum+1)
+            if subplotnum in [12,13,14,15]:
+                pylab.xlabel('[{:0.0f}adc]'.format(self.bins[1]-self.bins[0]))
+            if subplotnum in [0, 4, 8, 12]:
+                pylab.ylabel('[N]')
+            if subplotnum == 3:
+                pylab.title('{:0.0f} frames'.format(self._nentries[k]))
+
             self.drawone(k)
 
     def drawone(self,k):
-        pylab.bar(self.bins[:-1],self._histos[k],width=self.bins[1]-self.bins[0])
+        pylab.plot(self.bins[:-1],self._histos[k])#,width=self.bins[1]-self.bins[0])
         pylab.xlim(self.bins[0],self.bins[-1])
-        pylab.ylim(0.1,1.1*self._histos[k].max())
+        pylab.ylim(0.1,2*self._histos[k].max())
+        pylab.gca().set_yscale('log')
+        pylab.tick_params(axis='both', which='major', labelsize=6)
+        pylab.tick_params(axis='both', which='minor', labelsize=6)
+        pylab.gca().text(0.7,0.8,"tile {:0.0f}".format(k),fontsize=8,transform=pylab.gca().transAxes)
+        #pylab.gca().set_xticklabels(   [int(i.get_text())/20. for i in pylab.gca().get_xticklabels() if len(i.get_text())>0] )
 
 
 
@@ -50,7 +63,7 @@ def splittile(arr):
 
 class cspadhisto(event_process.event_process):
     def __init__(self):
-        self.bins = numpy.arange(-100,5000,200)
+        self.bins = numpy.arange(-100,800,50)
         self.histo = histo(64,self.bins)
         return
 
@@ -60,37 +73,57 @@ class cspadhisto(event_process.event_process):
 
     def event(self,evt):
         try:
-            cspad = evt.get(self.dev, self.src)
+            self.cspad = evt.get(self.dev, self.src,'calibrated')
         except Exception as e:
-            return
-        a = []
-        for i in range(0,4):
-            quad = cspad.quads(i)
-            d    = quad.data()
-            a.extend([ d[j] for j in range(0,8) ])
+            print e
+            return 0
 
-        for i, p in enumerate(a) :
+        if self.cspad is None:
+            print "no cspad"
+            return 0
+
+        for i, p in enumerate(self.cspad) :
             tile1, tile2 = splittile(p)
             self.histo.fill(2*i  ,tile1)
             self.histo.fill(2*i+1,tile2)
 
-        return
+        return 1
+
+    def showcspad(self,ax=None):
+        if not ax:
+            ax = pylab.figure().gca()
+        a = []
+        for i in range(0,4):
+            a.append( numpy.vstack( [cs.cspad[ i * j ] for j in range(0,8) ] ) )
+        rawframe = numpy.hstack(a)
+        ax.imshow(rawframe)
+
+        
 
 
 if __name__ == "__main__":
+    psana.setConfigFile("cspad_ndarray.cfg") # must be set before defining DataSource
     pylab.ion()
-    ds = psana.DataSource('exp=CXI/cxig1515:run=173')
-    psana.setConfigFile( 'cxicspad.cfg' )
+    ds = psana.DataSource('exp=CXI/cxig1515:run=47') # 51, 52, 54
+    #run=1
+    #ds = psana.DataSource('exp=cxi86415:run=%d'%run)
+    cls = ds.env().calibStore()
     cs = cspadhisto()
-    cs.set_stuff('DetInfo(CxiDs1.0:Cspad.0)' ,psana.CsPad.DataV2) # for this run use Ds2
+    cs.set_stuff('DetInfo(CxiDs1.0:Cspad.0)' ,psana.ndarray_float64_3) # for this run use Ds2
     #cs.set_stuff('cspad_mod.CsPadCalib' ,psana.CsPad.DataV2) # for this run use Ds2
     events = ds.events()
-    for i in xrange(100):
-        evt = events.next()
 
-    for i in xrange(30):
-        cs.event(evt)
+    cnt = 0
+    #for i in xrange(1000):
+    i = 0
+    for evt in events:
+        print i
+        i += 1
+        #evt = events.next()
+        cnt += cs.event(evt)
+        #if cnt >= 800:
+        #    break
 
-    #cs.histo.draw()
-    cs.histo.drawone(0)
+    cs.histo.draw()
+    #cs.histo.drawone(0)
 
