8ec23ee6e5e58beb532067e291ba3a918d93e307
Justin Garofoli <justing@slac.stanford.edu>
2015-03-05 17:22:23 -0800

    now reduces and histograms a steps

--- data_summary/evplib/cspad_histograms.py
+++ data_summary/evplib/cspad_histograms.py
@@ -6,6 +6,7 @@ import event_process
 import pylab
 from mpi4py import MPI
 from common import strtype
+import toolbox
 
 class result(object):
     """
@@ -20,34 +21,37 @@ class histo(object):
         self._histos = {}
         self._nentries = {}
         self._axes = {}
+        self._figs = []
         self.bins = bins
         for i in xrange(n):
             self._histos[i] = numpy.histogram([],bins=self.bins)[0]
-            self._nentries[i] = 0
+            self._nentries[i] = numpy.array([0])
         return
 
     def fill(self,i,arr):
         self._histos[i] += numpy.histogram(arr,bins=self.bins)[0]
-        self._nentries[i] += 1
+        self._nentries[i][0] += 1
         return
 
     def draw(self):
         for k in sorted(self._histos):
             subplotnum=k%16
             if k % 16 == 0:
-                pylab.figure()
+                self._figs.append(pylab.figure())
             self._axes[k] = pylab.subplot(4,4,subplotnum+1)
             if subplotnum in [12,13,14,15]:
                 pylab.xlabel('[{:0.0f}adc]'.format(self.bins[1]-self.bins[0]))
             if subplotnum in [0, 4, 8, 12]:
                 pylab.ylabel('[N]')
             if subplotnum == 3:
-                pylab.title('{:0.0f} frames'.format(self._nentries[k]))
+                pylab.title('{:0.0f} frames'.format(self._nentries[k][0]))
 
             self.drawone(k)
 
     def drawone(self,k):
-        pylab.plot(self.bins[:-1],self._histos[k])#,width=self.bins[1]-self.bins[0])
+        x,y = toolbox.mksteps(self.bins, self._histos[k]+0.1,startval=0.1) 
+        pylab.fill_between( x, 0.1, y[:-1] )
+        #pylab.plot(self.bins[:-1],self._histos[k])#,width=self.bins[1]-self.bins[0])
         pylab.xlim(self.bins[0],self.bins[-1])
         pylab.ylim(0.1,2*self._histos[k].max())
         pylab.gca().set_yscale('log')
@@ -63,6 +67,10 @@ def splittile(arr):
 
 class cspadhisto(event_process.event_process):
     def __init__(self):
+        self.logger                      = logging.getLogger(__name__+'.cspadhist')
+        self.output = event_process.event_process_output()
+        self.output['in_report']         = None
+        self.output['in_report_title']   = None
         self.bins = numpy.arange(-100,800,50)
         self.histo = histo(64,self.bins)
         return
@@ -70,24 +78,27 @@ class cspadhisto(event_process.event_process):
     def set_stuff(self,psana_src,psana_device,in_report=None,in_report_title=None):
         self.src         = psana.Source(psana_src)
         self.dev         = psana_device
+        self.output['in_report']         = in_report
+        self.output['in_report_title']   = in_report_title
 
     def event(self,evt):
         try:
+                              # type (not device)
             self.cspad = evt.get(self.dev, self.src,'calibrated')
         except Exception as e:
             print e
             return 0
 
         if self.cspad is None:
-            print "no cspad"
-            return 0
+            self.logger.info( "no cspad" )
+            return 
 
         for i, p in enumerate(self.cspad) :
             tile1, tile2 = splittile(p)
             self.histo.fill(2*i  ,tile1)
             self.histo.fill(2*i+1,tile2)
 
-        return 1
+        return 
 
     def showcspad(self,ax=None):
         if not ax:
@@ -97,6 +108,30 @@ class cspadhisto(event_process.event_process):
             a.append( numpy.vstack( [cs.cspad[ i * j ] for j in range(0,8) ] ) )
         rawframe = numpy.hstack(a)
         ax.imshow(rawframe)
+
+    def endJob(self):
+        self.logger.info('mpi reducing cspad histograms')
+        self.merged_histograms = histo(64,self.bins)
+
+        for k in sorted(self.merged_histograms._histos):
+            self.parent.comm.Reduce(self.histo._histos[k],   self.merged_histograms._histos[k],   op=MPI.SUM, root=self.reducer_rank)
+            self.parent.comm.Reduce(self.histo._nentries[k], self.merged_histograms._nentries[k], op=MPI.SUM, root=self.reducer_rank)
+
+        if self.parent.rank == self.reducer_rank:
+            self.logger.info("cspad histograms reducing")
+            self.output['text'].append("some text")
+
+            self.merged_histograms.draw()
+
+            self.output['figures'] = {}
+            for i, fig in enumerate(self.merged_histograms._figs) :
+                self.output['figures']['histos_{:0.0f}'.format(i)] = {}
+                fig.savefig(os.path.join( self.parent.output_dir, 'figure_cspad-histo-{:0.0f}.png'.format(i) ))
+                self.output['figures']['histos_{:0.0f}'.format(i)]['png'] = os.path.join( self.parent.output_dir, 'figure_cspad-histo-{:0.0f}.png'.format(i) )
+
+            self.parent.output.append(self.output)
+
+        return
 
         
 
